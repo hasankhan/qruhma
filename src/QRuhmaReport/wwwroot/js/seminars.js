ed272dc0480508f2994ce0df45e1484ac1b4136d
Muhammad Hasan Khan <slashdot10@gmail.com>
2017-02-18 12:02:38 -0800

    added torch bearers

--- src/QRuhmaReport/wwwroot/js/seminars.js
+++ src/QRuhmaReport/wwwroot/js/seminars.js
@@ -18,5 +18,6 @@
 	{ id: 1140, name: "Complicated?", title: "A to Z of Women's Modern Fiqh", date: "Apr 1st, 2016", instructor: "Shaykh Waleed Basyouni" },
 	{ id: 1171, name: "No Doubt", title: "God, Religion and Politics in the Modern World", date: "Sep 23rd, 2016", instructor: "Shaykh Yasir Qadhi" },
 	{ id: 1236, name: "Protect This House", title: "Al-Adab Al-Mufrad", date: "Dec 2nd, 2016", instructor: "Shaykh Yaser Birjas" },
-	{ id: 1274, name: "Shahadah", title: "The Fiqh of Da‘wah", date: "Feb 17, 2017", instructor: "Shaykh Kamal El Mekki" }
+	{ id: 1274, name: "Shahadah", title: "The Fiqh of Da‘wah", date: "Feb 17, 2017", instructor: "Shaykh Kamal El Mekki" },
+	{ id: 1320, name: "Torch Bearers", title: "Scholars of Islam", date: "May 12, 2017", instructor: "Shaykh Waleed Basyouni" }
 ];
